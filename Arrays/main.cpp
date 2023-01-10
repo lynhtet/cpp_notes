3a85a2a6fcc0e5b65eae3afc841e30c7a0abb253
Linn <linn.htet@outlook.com>
2023-01-11 00:46:14 +0800

    array_part1

---
+++ Arrays/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //* Array Notes
+    /**
+     * Fix Size, After declare, it couldn't change
+     * Array elements must be same datatypes
+     * There is no array bound checking
+     * Good pratise to always initialize the arrrays
+    */
+
+   //* Declaration and Initializing
+
+   //* Accessing Array Elements
+
+   //* Multidimensional Array 
+}
