06df8312c77a6d69a224fc6eb3b55d6316c92d31
Linn <linn.htet@outlook.com>
2023-01-10 22:48:04 +0800

    datatypes

---
+++ Datatypes/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //* PRIMITIVE DATATYPES
+    
+    // Characater Datatypes
+    char middle_initial {'j'};
+    cout << "Character: " << middle_initial << endl;
+
+    // Integer Datatypes
+    int x {18};
+    cout << "Integer: " << x << endl;
+
+    // Floating Points Datatypes
+    float payment {22.22};
+    cout << "Floating Point: "<< payment << endl;
+
+    // Boolean Types
+    bool is_correct {true};
+    cout << "Boolean: " << is_correct << endl;
+
+    return 0;
+}
